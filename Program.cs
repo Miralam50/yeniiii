f184b3dc163b0d89126c07debdb51d4f4801bfc8
Miralam Q. Safarov <miralamqs@code.local>
2023-05-13 14:35:36 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace myDemoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 1; i <= 10; i++)
+            {
+                if(i%2 == 0)
+                {
+                    Console.WriteLine(i + " " +  "cut");
+                }
+                else
+                {
+                    Console.WriteLine(i + " " + "tek");
+                }
+            }
+           
+
+        }
+    }
+}
